187372eeb10fb4a105c24718c66582a0b8bba04d
Mohamed Idbellaali <midbella@c3r7p1.1337.ma>
2024-08-19 14:22:01 +0100

    start

--- get_functins.py
+++ get_functins.py
@@ -1,7 +1,19 @@
 import os
 
 def get_files(DirectoryPath):
+	Files = []
 	Files  = os.listdir(DirectoryPath)
+	tab = [-1] * len(Files)
+	index = 0
+	for j in range(len(Files)):
+		if ".c" not in Files[j] and ".h" not in Files[j]:
+			tab[index] = j
+			index += 1
 	for i in range(len(Files)):
 		Files[i] = DirectoryPath + "/" + Files[i]
 	return (Files)
+
+def get_file_lines(FilePath):
+	data = open(FilePath, 'r')
+	for line in data:
+		print(line)
--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import get_functins
 import sys
 
 def main():
-	ProjectPath = input("INSERT THE PROJECT PATH: ")
-	print("INSERT THE PROJECT ALLOWED FUNCTIONS THEN PRESS CTR+D: ")
-	UserInput = sys.stdin.read()
+	ProjectPath = "../minishell"#input("INSERT THE PROJECT PATH: ")
+	# print("INSERT THE PROJECT ALLOWED FUNCTIONS THEN PRESS CTR+D: ")
+	# UserInput = sys.stdin.read()
+	UserInput = "readline, rl_clear_history, rl_on_new_line, rl_replace_line, rl_redisplay, add_history,printf, malloc,\n free, write, access, open, read,close, \nfork, wait, waitpid, wait3, wait4, signal,sigaction, sigemptyset, sigaddset, kill, exit,getcwd, chdir, stat, lstat, fstat, unlink, execve,dup, dup2, pipe, opendir, readdir, closedir,strerror, perror, isatty, ttyname, ttyslot, ioctl,getenv, tcsetattr, tcgetattr, tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs"
 	AllowedFunctions = UserInput.split(',')
 	for i in range(len(AllowedFunctions)):
 		AllowedFunctions[i] = AllowedFunctions[i].strip()
-	i += 1
-	print(AllowedFunctions)
-	print(get_functins.get_files(ProjectPath))
+	# print(AllowedFunctions)
+	ProjectFiles = get_functins.get_files(ProjectPath)
+	print(ProjectFiles)
+	# get_functins.get_file_lines(ProjectFiles[1])
 
 main()
